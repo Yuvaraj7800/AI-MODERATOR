f96013a7c72737df72f4e4fbb46438f0c3788890
tiwariavaneesh.king@gmail.com <tiwariavaneesh.king@gmail.com>
2023-05-02 16:21:31 +0530

    ..

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Admin = () => {
   return (
     <div>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Outlet/>
     </div>
   )
